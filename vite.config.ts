bbe21a66a7723d6527a326af5334dad1aa52985e
zhuddan <yobaci@163.com>
2024-08-24 04:19:07 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig, loadEnv } from 'vite'
 import React from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, './')
+export default defineConfig(() => {
   return {
     plugins: [
       React(),
     ],
-    server: {
-      port: Number(env.VITE_APP_PORT),
-      host: true,
-    },
     base: '/me/',
+    build: {
+      outDir: './docs/',
+    },
   }
 })
